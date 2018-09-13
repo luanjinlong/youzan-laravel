63a856499ca7fb501fd50dc83ebd75f4cef15923
xiaolong <646448149@qq.com>
2018-09-13 21:11:04 +0800

    update config

--- src/config.php
+++ src/config.php
@@ -4,11 +4,11 @@
  * 有赞配置文件
  */
 return [
-    'clientId' => env('YOUZAN_CLIENT_ID', 'e004da3d5081c400a'),
-    'clientSecret' => env('YOUZAN_CLIENT_SECRET_ID', '20a31b2ca029bba5c0567f69024dec27'),
+    'clientId' => env('YOUZAN_CLIENT_ID', ''),
+    'clientSecret' => env('YOUZAN_CLIENT_SECRET_ID', ''),
 
     // 店铺授权 ID 就是  kdt_id
-    'kdt_id' => env('YOUZAN_KDT_ID', '41066787'),
+    'kdt_id' => env('YOUZAN_KDT_ID', ''),
 ];
 
 
